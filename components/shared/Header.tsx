2e0234f5e0c07d5dff0522b9f6cdc75e6066d797
misha133kv <mihail_fomin_95@mail.ru>
2024-08-11 13:06:52 +0500

    Edited header layout

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -2,6 +2,8 @@ import { cn } from '@/lib/utils'
 import React from 'react'
 import Container from './Container'
 import Image from 'next/image'
+import { Button } from '../ui'
+import { ArrowRight, ShoppingCart, User } from 'lucide-react'
 
 type Props = {
   className: string
@@ -10,12 +12,37 @@ type Props = {
 function Header({className}: Props) {
   return (
     <header className={cn('', className)}>
-      <Container className='flex items-center justify-center py-8'>
+      <Container className='flex items-center justify-between py-8'>
         <div>
           <Image src='/logo.png' alt='Logo/' width={35} height={35} />
           <div>
             <h1 className='text-2xl uppercase font-black'>Next Pizza</h1>
             <p className='text-sm text-gray-400 leading-3'>вкусней уже некуда</p>
+          </div>
+        </div>
+
+        <div className='flex items-center gap-3'>
+          <Button
+            className='fkex items-center gap-1'
+            variant='outline'
+          >
+            <User size={16}/>
+            Войти
+          </Button>
+
+          <div>
+            <Button className='group relative'>
+              <b>520 P</b>
+              <span className='h-full w-[1px] bg-white/30 mx-3'/>
+              <div className='flex items-center gap-1 transition duration-300 group-hover:opacity-0'>
+                <ShoppingCart className='h-4 w-4 relative' strokeWidth={2}/>
+                <b>3</b>
+              </div>
+              <ArrowRight
+                size={20}
+                className='absolute right-5 transition duration-300 -translate-x-2 opacity-0 group-hover:opacity-100 group-hover:translate-x-0'
+              />
+            </Button>
           </div>
         </div>
       </Container>
