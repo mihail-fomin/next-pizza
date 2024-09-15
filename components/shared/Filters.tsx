204c2739a577582e8e2b408c9eb907768557402a
misha133kv <mihail_fomin_95@mail.ru>
2024-09-15 13:56:40 +0500

    Added filters

--- components/shared/Filters.tsx
+++ components/shared/Filters.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import { Title } from './Title'
 import { FilterCheckbox } from './FilterCheckBox'
+import { Input } from '../ui'
+import { RangeSlider } from './Range-slider'
+import CheckboxFiltersGroup from '../ui/checkbox-filters-group'
 
 type Props = {}
 
@@ -11,8 +14,39 @@ const Filters = (props: Props) => {
 
       <div className='flex flex-col gap-4'>
         <FilterCheckbox text='Можно собирать' value='1' />
-        <FilterCheckbox text='Можно собирать' value='2' />
+        <FilterCheckbox    text='Можно собирать' value='2' />
       </div>
+
+      <div className='mt-5 border-y border-y-neutral-100 py-6 pb-7'>
+        <div className='flex gap-3 mb-5'>
+          <Input type='number' placeholder='0' min={0} max={1000} defaultValue={0} />
+          <Input type='number' placeholder='1000' min={100} max={1000} defaultValue={500} />
+        </div>
+
+        <RangeSlider min={0} max={5000} step={10} value={[0, 5000]}/>
+      </div>
+
+      <CheckboxFiltersGroup
+        title='Ингредиенты'
+        className='mt-5'
+        limit={5}
+        defaultItems={[
+          { text: 'Пепперони', value: 'pepperoni' },
+          { text: 'Бекон', value: 'bacon' },
+          { text: 'Овощи', value:'vegetables' },
+          { text: 'Колбаса', value: 'sausage' },
+          { text: 'Сыр', value:'mozzarella' },
+          { text: 'Картофель', value: 'carrot' },
+        ]}
+        items = {[
+          { text: 'Пепперони', value: 'pepperoni' },
+          { text: 'Бекон', value: 'bacon' },
+          { text: 'Овощи', value:'vegetables' },
+          { text: 'Колбаса', value: 'sausage' },
+          { text: 'Сыр', value:'mozzarella' },
+          { text: 'Картофель', value: 'carrot' },
+        ]}
+      />
     </div>
   )
 }
---
+++ components/ui/checkbox-filters-group.tsx
@@ -0,0 +1,70 @@
+'use client'
+
+import React, { useState } from 'react'
+import { FilterChecboxProps, FilterCheckbox } from '../shared/FilterCheckBox'
+import { Input } from './input'
+
+type Item = FilterChecboxProps
+
+type Props = {
+  title: string
+  items: Item[]
+  defaultItems: Item[]
+  limit?: number
+  searchInputPlaceholder?: string
+  className?: string
+  onChange?: (values: string) => void
+  defaultValue?: string
+}
+
+export default function CheckboxFiltersGroup({
+  title,
+  items,
+  defaultItems,
+  limit = 5,
+  searchInputPlaceholder = 'Поиск...',
+  className,
+  onChange,
+  defaultValue,
+}: Props) { 
+  const [showAll, setShowAll] = useState(false)
+
+  const list = showAll ? items : defaultItems?.slice(0, limit)
+
+  return (
+    <div className={className}>
+      <p className='font-bold mb-3'>{title}</p>
+
+      {showAll && (
+        <div className='mb-5'>
+          <Input
+            placeholder={searchInputPlaceholder}
+            className='bg-gray-50 border-none'
+          />
+        </div>
+      )}
+
+      <div className='flex flex-col gap-4 max-h-96 pr-2 overflow-auto scrollbar'>
+        {list.map((item, index) => (
+          <FilterCheckbox
+            key={index}
+            text={item.text}
+            value={item.value}
+            endAdornment={item.endAdornment}
+            checked={false}
+            onCheckedChange={(ids) => console.log(ids)
+            }
+          />
+        ))}
+      </div>
+
+      {items.length > limit && (
+        <div className={showAll ? 'border-t border-t-neutral-100 mt-4' : '' }>
+          <button onClick={() => setShowAll(!showAll)} className='text-primary mt-3'>
+            {showAll ? 'Скрыть' : '+ Показать все'}
+          </button>
+        </div>
+      )}
+    </div>
+  )
+}
