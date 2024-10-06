44e8117f094d305baedd17f65c96f9b60c4ddc1a
misha133kv <mihail_fomin_95@mail.ru>
2024-10-06 18:51:37 +0500

    Added Prpduct card

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Container from "@/components/shared/Container";
 import Filters from "@/components/shared/Filters";
+import ProductCard from "@/components/shared/ProductCard";
 import { Title } from "@/components/shared/Title";
 import TopBar from "@/components/shared/TopBar";
 
@@ -22,8 +23,12 @@ export default function Home() {
           <div className="flex-1">
             <div className="flex flex-col gap-16">
               Список товаров
-            {/* <ProductsGroupList title='Пиццы' items={[1, 2, 3, 4, 5]}/>
-            <ProductsGroupList title='Комбо' items={[1, 2, 3, 4, 5]}/> */}
+              <ProductCard
+                id={1}
+                name="Чизбургер-пицца"
+                price={550}
+                imageUrl="https://media.dodostatic.net/image/r:292x292/11EE7D6175C10773BFE36E56D48DF7E3.avif"
+              />
             </div>
           </div>
         </div>
---
+++ components/shared/ProductCard.tsx
@@ -0,0 +1,44 @@
+import Link from 'next/link'
+import React from 'react'
+import { Title } from './Title';
+import { Button } from '../ui';
+import { Plus } from 'lucide-react';
+
+type Props = {
+  id: number;
+  name: string;
+  price: number;
+  imageUrl: string;
+  className?: string;
+}
+
+const ProductCard: React.FC<Props> = ({ id, name, price, imageUrl, className }) => {
+  return (
+    <div>
+      <Link href = {`/product/${id}`}>
+        <div className='flex justify-center p-6 bg-secondary rounded-lg h-[260px]'>
+          <img className='w-[215px] h-[215px]' src={imageUrl} alt={name}/>
+        </div>
+
+        <Title text={name} size='sm' className='mb-1 mt-3 font-bold'/>
+
+        <p className='text-sm text-gray-400'>
+          Ципленок, моцарелла, серы чеддер и пармезан
+        </p>
+
+        <div className='flex justify-between items-center mt-4'>
+          <span className='text-xl'>
+            от <b>{price}</b> руб.
+          </span>
+
+          <Button variant='secondary' className='text-base font-bold'>
+            <Plus size={20} className='mr-1' />
+            Добавить
+          </Button>
+        </div>
+      </Link>
+    </div>
+  )
+}
+
+export default ProductCard
