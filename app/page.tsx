8007f4cac85d1460b713b78a555e0a43d2b42ab0
misha133kv <mihail_fomin_95@mail.ru>
2024-10-26 14:49:58 +0500

    Added Zustand

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Container from "@/components/shared/Container";
 import Filters from "@/components/shared/Filters";
-import ProductCard from "@/components/shared/ProductCard";
+import ProductsGroup from "@/components/shared/ProductsGroup";
 import { Title } from "@/components/shared/Title";
 import TopBar from "@/components/shared/TopBar";
 
@@ -14,7 +14,7 @@ export default function Home() {
       <TopBar />
 
       <Container className="mt-10 pb-14">
-        <div className="flex gap-[60px]">
+        <div className="flex gap-[80px]">
 
           <div className="w-[250px]">
             <Filters />
@@ -22,12 +22,32 @@ export default function Home() {
 
           <div className="flex-1">
             <div className="flex flex-col gap-16">
-              Список товаров
-              <ProductCard
-                id={1}
-                name="Чизбургер-пицца"
-                price={550}
-                imageUrl="https://media.dodostatic.net/image/r:292x292/11EE7D6175C10773BFE36E56D48DF7E3.avif"
+              <ProductsGroup
+                title="Пиццы"
+                items={[
+                  { id: 1, name: 'Чизбургер-пицца', price: 550, imageUrl: 'https://media.dodostatic.net/image/r:292x292/11EE7D6175C10773BFE36E56D48DF7E3.avif', items: [{ price: 550 }], },
+                  { id: 2, name: 'Чизбургер-пицца', price: 550, imageUrl: 'https://media.dodostatic.net/image/r:292x292/11EE7D6175C10773BFE36E56D48DF7E3.avif', items: [{ price: 550 }], },
+                  { id: 3, name: 'Чизбургер-пицца', price: 550, imageUrl: 'https://media.dodostatic.net/image/r:292x292/11EE7D6175C10773BFE36E56D48DF7E3.avif', items: [{ price: 550 }], },
+                  { id: 3, name: 'Чизбургер-пицца', price: 550, imageUrl: 'https://media.dodostatic.net/image/r:292x292/11EE7D6175C10773BFE36E56D48DF7E3.avif', items: [{ price: 550 }], },
+                  { id: 3, name: 'Чизбургер-пицца', price: 550, imageUrl: 'https://media.dodostatic.net/image/r:292x292/11EE7D6175C10773BFE36E56D48DF7E3.avif', items: [{ price: 550 }], },
+                  { id: 3, name: 'Чизбургер-пицца', price: 550, imageUrl: 'https://media.dodostatic.net/image/r:292x292/11EE7D6175C10773BFE36E56D48DF7E3.avif', items: [{ price: 550 }], },
+                ]}
+                categoryId={1}
+                listCassName=""
+              />
+
+              <ProductsGroup
+                title="Завтраки"
+                items={[
+                  { id: 1, name: 'Чизбургер-пицца', price: 550, imageUrl: 'https://media.dodostatic.net/image/r:292x292/11EE7D6175C10773BFE36E56D48DF7E3.avif', items: [{ price: 550 }], },
+                  { id: 2, name: 'Чизбургер-пицца', price: 550, imageUrl: 'https://media.dodostatic.net/image/r:292x292/11EE7D6175C10773BFE36E56D48DF7E3.avif', items: [{ price: 550 }], },
+                  { id: 3, name: 'Чизбургер-пицца', price: 550, imageUrl: 'https://media.dodostatic.net/image/r:292x292/11EE7D6175C10773BFE36E56D48DF7E3.avif', items: [{ price: 550 }], },
+                  { id: 4, name: 'Чизбургер-пицца', price: 550, imageUrl: 'https://media.dodostatic.net/image/r:292x292/11EE7D6175C10773BFE36E56D48DF7E3.avif', items: [{ price: 550 }], },
+                  { id: 5, name: 'Чизбургер-пицца', price: 550, imageUrl: 'https://media.dodostatic.net/image/r:292x292/11EE7D6175C10773BFE36E56D48DF7E3.avif', items: [{ price: 550 }], },
+                  { id: 6, name: 'Чизбургер-пицца', price: 550, imageUrl: 'https://media.dodostatic.net/image/r:292x292/11EE7D6175C10773BFE36E56D48DF7E3.avif', items: [{ price: 550 }], },
+                ]}
+                categoryId={2}
+                listCassName=""
               />
             </div>
           </div>
--- components/shared/Categorites.tsx
+++ components/shared/Categorites.tsx
@@ -1,24 +1,36 @@
+'use client'
+
 import { cn } from '@/lib/utils'
+import { useCategoryStore } from '@/store/category'
 import React from 'react'
 
 type Props = {
   className?: string
 }
 
-const categories = ['Пицца', 'Салаты', 'Десерты', 'Комбо', 'Закуски', 'Коктейли', 'Десерты', 'Кофе']
-const activeIndex = 0
-
+const categories: { id: number; name: string }[] = [
+  { id: 1, name: 'Пиццы' },
+  { id: 2, name: 'Завтраки' },
+  { id: 3, name: 'Десерты' },
+  { id: 4, name: 'Комбо' },
+  { id: 5, name: 'Закуски' },
+  { id: 6, name: 'Коктейли' },
+  { id: 7, name: 'Десерты' },
+  { id: 8, name: 'Кофе' },
+]
 const Categorites = ({ className}: Props) => {
+  const categoryActiveId = useCategoryStore((state) => state.activeId)
+
   return (
     <div className={cn('inline-flex gap-1 bg-gray-50 p-1 rounded-2xl', className)}>
       {
-        categories.map((category, index) => (
+        categories.map(({name, id}, index) => (
           <a className={cn(
             'flex items-center font-bold h-11 rounded-2xl px-5',
-            index === activeIndex && 'bg-white shadow-md shadow-gray-200 text-primary',
+            categoryActiveId === id && 'bg-white shadow-md shadow-gray-200 text-primary',
           )} key={index}>
             <button>
-              {category}
+              {name}
             </button>
           </a>
         ))
