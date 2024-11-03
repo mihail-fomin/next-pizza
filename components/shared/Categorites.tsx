85a6475cf481f7b2fd37980c8a7348d3d1e81e0f
misha133kv <mihail_fomin_95@mail.ru>
2024-11-03 14:48:24 +0500

    link

--- components/shared/Categorites.tsx
+++ components/shared/Categorites.tsx
@@ -25,10 +25,14 @@ const Categorites = ({ className}: Props) => {
     <div className={cn('inline-flex gap-1 bg-gray-50 p-1 rounded-2xl', className)}>
       {
         categories.map(({name, id}, index) => (
-          <a className={cn(
-            'flex items-center font-bold h-11 rounded-2xl px-5',
-            categoryActiveId === id && 'bg-white shadow-md shadow-gray-200 text-primary',
-          )} key={index}>
+          <a 
+            key={index}
+            className={cn(
+              'flex items-center font-bold h-11 rounded-2xl px-5',
+              categoryActiveId === id && 'bg-white shadow-md shadow-gray-200 text-primary',
+            )} 
+            href={`/#${name}`}
+          >
             <button>
               {name}
             </button>
