d026f3b11fe938400e2bc61002b9821cfbf6a865
laptop <107790477+mihail-fomin@users.noreply.github.com>
2024-11-21 16:19:16 +0500

    Updated seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -133,10 +133,48 @@ async function up() {
             generateProductItem({ productId: 17 }),
           ],
     })
+
+    await prisma.cart.createMany({
+        data: [
+            { userId: 1, totalAmount: 0, token: '1111' },
+            { userId: 2, totalAmount: 0, token: '2222' },
+        ]
+    })
+
+    await prisma.cartItem.create({
+        data: {
+                productItemId: 1,
+                cartId: 1,
+                quantity: 2,
+                ingredients: {
+                    connect:[ { id: 1 }, { id: 2 }, { id: 3 }, { id: 4} ]
+                }
+            }
+
+    })
 }
 
 async function down() {
+    const tables = [
+        'User',
+        'Category',
+        'Product',
+        'ProductItem',
+        'Ingredient',
+        'Cart',
+        'CartItem',
+    ]
+
+    // tables.forEach(async (table) => {
+    //     await prisma.$executeRaw`TRUNCATE TABLE "${table}" RESTART IDENTITY CASCADE`
+    // })
     await prisma.$executeRaw`TRUNCATE TABLE "User" RESTART IDENTITY CASCADE`
+    await prisma.$executeRaw`TRUNCATE TABLE "Category" RESTART IDENTITY CASCADE`
+    await prisma.$executeRaw`TRUNCATE TABLE "Product" RESTART IDENTITY CASCADE`
+    await prisma.$executeRaw`TRUNCATE TABLE "ProductItem" RESTART IDENTITY CASCADE`
+    await prisma.$executeRaw`TRUNCATE TABLE "Ingredient" RESTART IDENTITY CASCADE`
+    await prisma.$executeRaw`TRUNCATE TABLE "Cart" RESTART IDENTITY CASCADE`
+    await prisma.$executeRaw`TRUNCATE TABLE "CartItem" RESTART IDENTITY CASCADE`
 }
 
 async function main() {
